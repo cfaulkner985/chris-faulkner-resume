c0e796846ebffd3de33723b08ff5effd5cd611f9
cfaulkner985 <cfaulkner985@gmail.com>
2020-12-22 10:58:21 +0000

    Finishiong touches

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,8 +1,8 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_4uhxht8","template_01i68zq", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "message": contactForm.message.value
     })
     .then(
         function(response) {
